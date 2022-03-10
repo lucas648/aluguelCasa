9a05829f52756d3f5848d971fc1db31649c37b92
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-03-10 11:48:52 -0300

    Finalizando a tela Home

---
+++ src/components/Recomended.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, ImageBackground, Text } from "react-native"
+
+export default function New(props){
+  return (
+    <ImageBackground
+      source={props.cover}
+      style={styles.container}
+      blurRadius={5}
+    >
+      <Text style={[styles.house, styles.shadow]}>
+        {props.houseName}
+      </Text>
+      <Text style={[styles.description, styles.shadow]}>
+        {props.offer}
+      </Text>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    height: 130,
+    width: 230,
+    marginRight: 20,
+    marginBottom: 40,
+    opacity: 0.8,
+    backgroundColor: '#000',
+    marginLeft: 3,
+    padding: 12,
+    marginTop: 20
+  },
+  house:{
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 15,
+  },
+  description:{
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 12
+  },
+  shadow:{
+    textShadowOffset: {width: 1, height: 2},
+    textShadowRadius: 3,
+    textShadowColor: '#000'
+  }
+});
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,11 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { ScrollView, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import * as Icon from "react-native-feather";
 
 import New from "../components/New";
-
-import * as Icon from "react-native-feather";
+import House from "../components/House"
+import Recomended from "../components/Recomended"
 
 export default function Home(){
 
@@ -29,7 +30,7 @@ export default function Home(){
       <View style={styles.contentView}>
         <Text style={styles.title}>Novidades</Text>
       </View>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{paddingHorizontal: 15}}>
         <New
           cover={require('../assets/house1.jpg')}
           name="Casa de Praia"
@@ -46,10 +47,72 @@ export default function Home(){
         />
         <New
           cover={require('../assets/house3.jpg')}
-          name="Casa São Paulo"
+          name="Casa Alphavile"
           description="Perto de tudo, completa e bem equipada com tudo que você precisa"
           price="2.500,90"
           onPress={()=>{}}
+        />
+      </ScrollView>
+
+      <View style={
+        {
+          flexDirection: 'row', 
+          marginBottom: 10, 
+          alignItems: 'center'
+        }
+      }>
+        <Text style={
+          [
+            styles.title, 
+            { 
+              marginTop: 20
+            }
+          ]
+        }>
+          Próximo de Você!!
+        </Text>
+      </View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{paddingHorizontal: 15}}>
+        <House 
+          cover={require('../assets/house4.jpg')}
+          description={'Esta casa novinha em Ubatuba está esperando por você agora mesmo'}
+          price={'1.456,35'}
+        />
+        <House 
+          cover={require('../assets/house5.jpg')}
+          description={'Uma casa única em região nobre do Rio de Janeiro, bem espaçosa e confortável'}
+          price={'1.207,00'}
+        />
+        <House 
+          cover={require('../assets/house6.jpg')}
+          description={'Casa única em Recife, próximo a pontos históricos e da melhor praia da cidade'}
+          price={'956,88'}
+        />
+      </ScrollView>
+
+      <Text style={[
+        styles.title, 
+        { 
+          marginTop: 20
+          }
+      ]}>
+        Dica do Dia
+      </Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{paddingHorizontal: 15}}>
+        <Recomended
+          cover={require('../assets/house4.jpg')}
+          houseName={'Casa alto padrão'}
+          offer={'30% off'}
+        />
+        <Recomended
+          cover={require('../assets/house6.jpg')}
+          houseName={'Apartamento Jardins'}
+          offer={'25% off'}
+        />
+        <Recomended
+          cover={require('../assets/house2.jpg')}
+          houseName={'Casa de campo'}
+          offer={'65% off'}
         />
       </ScrollView>
     </ScrollView>
